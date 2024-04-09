fda509e3db4ce311967714f2fe4c36ba1423d142
NickolasKemp <kemplent@gmail.com>
2024-04-09 08:40:12 +0300

    add base url

--- frontend/src/api/interceptors.ts
+++ frontend/src/api/interceptors.ts
@@ -1,7 +1,7 @@
 import axios, {type CreateAxiosDefaults} from 'axios';
 
 const options:CreateAxiosDefaults = {
-  baseURL: process.env.SERVER_URL,
+  baseURL: "https://task-managment-app-henna.vercel.app/api",
   headers: {
     "Content-Type" : 'application/json'
   }
